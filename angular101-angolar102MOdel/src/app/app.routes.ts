615f6db654bfc11e163c42dfcd8e3269286313b2
Tiepeter777 <idkaimak01@gmail.com>
2025-04-08 08:56:06 +0700

    up

--- angular101-angolar102MOdel/src/app/app.routes.ts
+++ angular101-angolar102MOdel/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { TestComponent } from './componet/test/test.component';
 import { DowndloadComponent } from './componet/downdload/downdload.component';
 import { CustomerComponent } from './componet/getcustomer/customer.component';
 import { FormComponent } from './componet/form/form.component';
@@ -7,15 +6,11 @@ import { GetbyidComponent } from './componet/getbyid/getbyid.component';
 
 export const routes: Routes = [
     {
-        path: 'testRoute/:id',  // รับค่า id จาก URL
-        component: TestComponent,
-        title: 'Home Page'
-    },{
         path: 'Download/:id',  // รับค่า id จาก URL
         component: DowndloadComponent,
         title: 'Home Page'
     },{
-        path: 'getCustomer',  // รับค่า id จาก URL
+        path: '',  // รับค่า id จาก URL
         component: CustomerComponent,
         title: 'Home Page'
     }   ,{
